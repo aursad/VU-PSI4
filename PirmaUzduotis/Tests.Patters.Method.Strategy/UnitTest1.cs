caa77ed2015481a5bdfe9066d8165545eaafc0e1
Aurimas Sadauskas <aurimas.sadauskas@gmail.com>
2014-03-20 18:02:42 +0200

    Init file hierarchy
    Patterns and tests init

---
+++ PirmaUzduotis/Tests.Patters.Method.Strategy/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Patters.Method.Strategy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
